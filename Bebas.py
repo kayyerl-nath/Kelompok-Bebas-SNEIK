ec61b6c3b7fa41e2c594182b3971ccc6990e2799
Joooveeee <joveeranuntu026@student.unsrat.ac.id>
2025-11-26 23:33:11 +0800

    Update Bebas.py

--- Bebas.py
+++ Bebas.py
@@ -14,27 +14,27 @@ BACKGROUND_COLOR = "#000000"
 class Snake:
   
   def __init__(self):
-        self.body_size = BODY_PARTS
-        self.coordinates = []
-        self.squares = []
+      self.body_size = BODY_PARTS
+      self.coordinates = []
+      self.squares = []
 
-        for i in range(0, BODY_PARTS):
-            self.coordinates.append([0, 0])
+      for i in range(0, BODY_PARTS):
+          self.coordinates.append([0, 0])
 
-        for x, y in self.coordinates:
-            square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR, tag="snake")
-            self.squares.append(square)
+      for x, y in self.coordinates:
+          square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR, tag="snake")
+          self.squares.append(square)
 
 
 class Food:
   def __init__(self):
 
-        x = random.randint(0, (GAME_WIDTH // SPACE_SIZE) - 1) * SPACE_SIZE
-        y = random.randint(0, (GAME_HEIGHT // SPACE_SIZE) - 1) * SPACE_SIZE
+      x = random.randint(0, (GAME_WIDTH // SPACE_SIZE) - 1) * SPACE_SIZE
+      y = random.randint(0, (GAME_HEIGHT // SPACE_SIZE) - 1) * SPACE_SIZE
 
-        self.coordinates = [x, y]
+      self.coordinates = [x, y]
 
-        canvas.create_oval(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=FOOD_COLOR, tag="food")
+      canvas.create_oval(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=FOOD_COLOR, tag="food")
 
 def next_turn(snake, food):
 
