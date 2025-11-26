917f1d3fda91daa804c1065bd1cbf792b8761d36
anggunfitriana <fitrianaanggun54@gmail.com>
2025-11-26 17:51:21 +0700

    Update Bebas.py

--- Bebas.py
+++ Bebas.py
@@ -24,6 +24,9 @@ def change_direction(new_direction):
 def check_collisions(snake):
 
 def game_over():
+    canvas.delete(ALL)
+    canvas.create_text(canvas.winfo_width()/2, canvas.winfo_height()/2,
+                       font=('consolas',70), text="GAME OVER", fill="red", tag="gameover")
 
 
 
