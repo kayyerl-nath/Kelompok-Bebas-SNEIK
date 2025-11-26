678a476392f1f0a1637af1227a9276c998873739
joveeranuntu <joveeranuntu026@student.unsrat.ac.id>
2025-11-26 18:25:02 +0800

    Update Bebas.py

--- Bebas.py
+++ Bebas.py
@@ -21,6 +21,21 @@ def next_turn(snake, food):
 
 def change_direction(new_direction):
 
+    global direction
+
+    if new_direction == 'left':
+        if direction != 'right':
+            direction = new_direction
+    elif new_direction == 'right':
+        if direction != 'left':
+            direction = new_direction
+    elif new_direction == 'up':
+        if direction != 'down':
+            direction = new_direction
+    elif new_direction == 'down':
+        if direction != 'up':
+            direction = new_direction
+
 def check_collisions(snake):
 
 def game_over():
